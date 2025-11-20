288fdd16287919c0e603656ef5c599a17d523e47
Ashley Fitzgerald <ashleyf1996@gmail.com>
2025-11-20 13:18:58 +0000

    FEAT: Added getter method to control attribute access

--- enemy.py
+++ enemy.py
@@ -1,5 +1,21 @@
 class Enemy:
-    type_of_enemy: str 
-    health_points: int = 10
-    attack_damage: int = 1
 
+    def __init__(self, type_of_enemy, health_ponts, attack_damage):
+      #instatiate new object with these values
+      #this new object wi
+      self.__type_of_enemy = type_of_enemy
+      self.health_points = health_ponts
+      self.attack_damage = attack_damage
+      
+
+    def talk(self):
+        print(f'I am a {self.__type_of_enemy} be prepared to fight!!')
+
+    def walk_forward(self):
+     print(f'{self.__type_of_enemy} moves closer to you')
+
+    def attack(self):
+        print(f'{self.__type_of_enemy} attacks for {self.attack_damage} damage')
+
+    def get_type_of_enemy(self):
+       return self.__type_of_enemy
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from enemy import *
-enemy = Enemy()
 
-enemy.type_of_enemy = "Zombie"
-print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do attack of {enemy.attack_damage} ')
+
+zombie = Enemy('Zombie', 1, 10)
+
+
+print(zombie.get_type_of_enemy())
