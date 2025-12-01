f764c9c91998cce9b9fde0e16ceec98484d16710
Ashley Fitzgerald <ashleyf1996@gmail.com>
2025-12-01 10:51:58 +0000

    FEAT: Made hero and weapon class

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from enemy import *
 from zombie import *
 from ogre import *
+from hero import * 
+from weapon import *
+
 
 def battle(e1: Enemy, e2: Enemy):
     e1.talk()
@@ -25,10 +28,30 @@ def battle(e1: Enemy, e2: Enemy):
         print(f'{e2.get_type_of_enemy()} wins!!')
     
 
+def hero_battle(hero : Hero, enemy: Enemy):
+
+    while hero.health_points > 0 and enemy.health_points > 0:
+        print("------------")
+      
+        print(f'Hero {hero.health_points} HP left')
+        print(f'{enemy.get_type_of_enemy()}: {enemy.health_points} HP left')
+        enemy.attack()
+        hero.health_points -= enemy.attack_damage
+        hero.attack()
+        enemy.health_points -= hero.attack_damage
+    print("------------")
+    print("------------")
+
+    if hero.health_points > 0:
+        print(f'{hero.get_type_of_enemy()} wins!!')
+    else: 
+        print(f'{enemy.get_type_of_enemy()} wins!!')
+    
 
 
 zombie = Zombie(10,1)
 ogre = Ogre(20,3)
+hero = Hero(10,1)
 
-battle(zombie, ogre) 
+hero_battle(hero, zombie) 
 
